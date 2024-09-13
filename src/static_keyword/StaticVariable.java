5807c6497461d7280be4e6f915084e31a0c56a6a
unknown <ssiam8474@gmail.com>
2024-09-13 16:46:29 +0600

    static class using

--- src/static_keyword/StaticVariable.java
+++ src/static_keyword/StaticVariable.java
@@ -1,5 +1,17 @@
 package static_keyword;
 
 public class StaticVariable {
-    static String universityName= "United International University";
+//    static String universityName= "United International University";
+
+
+    static int count= 0;
+
+    StaticVariable(){
+        count++;
+    }
+
+    void totalCount(){
+        System.out.println("Count value is: " + count);
+    }
+
 }
--- src/static_keyword/StaticVariableTest.java
+++ src/static_keyword/StaticVariableTest.java
@@ -6,6 +6,17 @@ public class StaticVariableTest {
 //        System.out.println(std1.universityName);
 
 //When I call static variable, I don't have to make any object. I direct call the class name than variable
-        System.out.println(StaticVariable.universityName);
+//        System.out.println(StaticVariable.universityName);
+
+
+
+        StaticVariable str1= new StaticVariable();
+        str1.totalCount();
+        StaticVariable str2= new StaticVariable();
+        str2.totalCount();
+        StaticVariable str3= new StaticVariable();
+        str3.totalCount();
+        StaticVariable str4= new StaticVariable();
+        str4.totalCount();
     }
 }
