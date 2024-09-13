217214bc854ef410d2140f3f05336b56f7738a45
unknown <ssiam8474@gmail.com>
2024-09-13 17:23:06 +0600

    Restriction of static method

--- src/static_method/StaticMethodDemo.java
+++ src/static_method/StaticMethodDemo.java
@@ -1,11 +1,36 @@
+//Static Method
+
+//package static_method;
+//
+//public class StaticMethodDemo {
+//    void display1(){
+//        System.out.println("This is not a static method.");
+//    }
+//
+//    static void display2(){
+//        System.out.println("This is a static method.");
+//    }
+//}
+
+
+
+
+
+
+
+//Static Method restrictions
+
 package static_method;
 
 public class StaticMethodDemo {
+    static int x= 34;
     void display1(){
+        display2();
         System.out.println("This is not a static method.");
     }
 
     static void display2(){
+        System.out.println("Value of X is: " + x);
         System.out.println("This is a static method.");
     }
 }
--- src/static_method/StaticMethodDemoTest.java
+++ src/static_method/StaticMethodDemoTest.java
@@ -1,12 +1,32 @@
+//Static Method
+
+//package static_method;
+//
+//public class StaticMethodDemoTest {
+//    public static void main(String[] args) {
+////        Creating object for non-static method
+//        StaticMethodDemo a1= new StaticMethodDemo();
+//        a1.display1();
+//
+////        Call Class for static Method
+//        StaticMethodDemo.display2();
+//    }
+//}
+
+
+
+
+
+
+//Static Method restrictions
+
 package static_method;
 
 public class StaticMethodDemoTest {
     public static void main(String[] args) {
-//        Creating object for non-static method
         StaticMethodDemo a1= new StaticMethodDemo();
         a1.display1();
 
-//        Call Class for static Method
         StaticMethodDemo.display2();
     }
 }
